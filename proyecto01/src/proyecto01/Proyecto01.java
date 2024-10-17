f735ad6671ce28fe5cc115edfad5fc989ae26c2b
andresvallejoT-T <andresvallejo102006@gmail.com>
2024-10-17 12:37:48 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,11 @@
+
+package proyecto01;
+
+public class Proyecto01 {
+
+
+    public static void main(String[] args) {
+    System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
